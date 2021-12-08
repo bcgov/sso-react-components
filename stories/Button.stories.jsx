835e153eccb5046c353f6d3620038195a2a448e5
Jonathan Langlois <jon@button.is>
2021-12-08 11:02:23 -0800

    chore: comments
    remove redundant comments from code

--- stories/Button.stories.jsx
+++ stories/Button.stories.jsx
@@ -1,31 +1,26 @@
 import React from 'react';
 import Button from 'src/components/Button';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Button',
   component: Button,
 };
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Button {...args}>{args.text}</Button>;
 
 export const Callout = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Callout.args = {
   variant: 'callout',
   text: '+ Request Integration',
 };
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   variant: 'primary',
   text: 'Send Invitation',
 };
 
 export const Secondary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Secondary.args = {
   variant: 'secondary',
   text: 'Cancel',
