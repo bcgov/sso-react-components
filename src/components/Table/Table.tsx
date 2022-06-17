e9830d7bb9499b27ea4b037cff126c6441886d75
Junmin Ahn <junminahn@outlook.com>
2022-06-17 12:18:30 -0700

    chore: update pre-commit hooks

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+export const TABLE_BACKGROUND_COLOR = '#ededed';
+export const TABLE_ROW_ACTIVE_COLOR = '#4950FA';
+export const TABLE_ROW_HEIGHT = 40;
+export const TABLE_ROW_HEIGHT_MINI = 40;
+export const TABLE_ROW_SPACING = 5;
+export const TABLE_ROW_BORDER_RADIUS = 6;
+
+const Table = styled.table<{ variant?: string; readOnly?: boolean }>`
+  width: 100%;
+  -webkit-box-shadow: none;
+  background-color: ${TABLE_BACKGROUND_COLOR};
+  padding: 0.1em 0.6em;
+  box-shadow: none;
+  text-align: left;
+  border-collapse: separate;
+  border-spacing: 0 ${TABLE_ROW_SPACING}px;
+  color: black;
+
+  & thead {
+    font-size: 16px;
+    & > th {
+      min-width: ${(props) => (props.variant === 'mini' ? '30px' : '140px')};
+    }
+  }
+
+  & tbody {
+    font-size: ${(props) => (props.variant === 'mini' ? '14px' : '16px')};
+    & > tr {
+      height: ${(props) => (props.variant === 'mini' ? `${TABLE_ROW_HEIGHT_MINI}px` : `${TABLE_ROW_HEIGHT}px`)};
+      background-color: #fff;
+
+      td:first-child {
+        border-top-left-radius: ${TABLE_ROW_BORDER_RADIUS}px;
+      }
+      td:last-child {
+        border-top-right-radius: ${TABLE_ROW_BORDER_RADIUS}px;
+      }
+
+      td:first-child {
+        border-bottom-left-radius: ${TABLE_ROW_BORDER_RADIUS}px;
+      }
+      td:last-child {
+        border-bottom-right-radius: ${TABLE_ROW_BORDER_RADIUS}px;
+      }
+
+      ${(props) =>
+        !props.readOnly &&
+        `
+        &:hover {
+          background-color: ${TABLE_ROW_ACTIVE_COLOR};
+          color: #fff;
+          cursor: pointer;
+        }
+        &.active {
+          background-color: ${TABLE_ROW_ACTIVE_COLOR};
+          color: #fff;
+          font-weight: bold;
+        }
+      `}
+    }
+  }
+
+  & th:first-child,
+  & td:first-child {
+    padding-left: 1em;
+    text-align: left;
+  }
+
+  & th,
+  & td {
+    border: none;
+    padding: 0;
+    overflow: hidden;
+  }
+`;
+
+export default Table;
