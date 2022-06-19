cc7451c5e64ab515840cb10312c546a30ae43a11
Junmin Ahn <junminahn@outlook.com>
2022-06-19 16:10:38 -0700

    chore: expose Table component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Accordion } from './Accordion';
 export { default as NumberedContents } from './NumberedContents';
 export { default as Button } from './Button';
 export { default as Alert } from './Alert';
+export { default as Table } from './Table';
