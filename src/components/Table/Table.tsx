74759f6a78835c9059317356001ca60c021a62da
ZHE CHEN <kzsyixi@gmail.com>
2022-07-20 15:32:25 -0700

    chore: fix table alignment, fix typo

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -69,6 +69,15 @@ const Table = styled.table<{ variant?: string; readOnly?: boolean }>`
     text-align: left;
   }
 
+  & th:last-child {
+    text-align: right;
+    padding-right: 1em;
+  }
+  & td:last-child {
+    text-align: right;
+    padding-right: 2em;
+  }
+
   & th,
   & td {
     border: none;
