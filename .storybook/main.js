8cc9f03d67625a198290368422eaa5f2f100135f
Nithin Shekar Kuruba <81444731+NithinKuruba@users.noreply.github.com>
2023-03-31 11:23:53 -0700

    chore: using vite builder (#80)
    * chore: using vite builder
    
    * chore: removed loader-utils lib
    
    * chore: removed babel-loader to fix issue with loader-utils

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,29 @@
 const path = require('path');
+const { mergeConfig } = require('vite');
+const viteTsConfigPaths = require('vite-tsconfig-paths').default;
 
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-addon-designs', '@storybook/addon-a11y'],
-  framework: '@storybook/react',
-  webpackFinal: async (config, { configType }) => {
-    config.resolve.modules.push(path.resolve(__dirname, '../'));
-
-    return config;
+  framework: '@storybook/react-vite',
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      // Use the same "resolve" configuration as your app
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, './'),
+          src: path.resolve(__dirname, '../src'),
+        },
+        dedupe: ['@storybook/client-api'],
+      },
+      plugins: [
+        viteTsConfigPaths({
+          root: '../',
+        }),
+      ],
+    });
   },
   core: {
-    builder: 'webpack5',
+    builder: '@storybook/builder-vite',
   },
 };
